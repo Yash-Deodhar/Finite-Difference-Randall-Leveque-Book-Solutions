70b58701212415f0071ae53aee2bff2d24bbcfeb
Yash-Deodhar <114303252+Yash-Deodhar@users.noreply.github.com>
2025-03-26 17:01:19 -0500

    Create 1_2a.m

---
+++ Chapter 1/1_2a.m
@@ -0,0 +1,10 @@
+function c = fdcoeffV(k, xbar, x)
+n = length(x);
+rhs = zeros(n,1);
+rhs(k+1) = 1;
+lhs = zeros(n,n);
+for i = 1:n
+    lhs(i,:) = (1/factorial(i-1))*(x(:) - xbar).^(i-1);
+end
+c = lhs\rhs;
+c = c';
